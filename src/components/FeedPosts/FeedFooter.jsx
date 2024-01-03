360e60cdc140fb6f791e21e65aeadd650354ea51
NewtonKamau <kamaunewton78@gmail.com>
2024-01-03 15:13:54 +0300

    designed the footer

--- src/components/FeedPosts/FeedFooter.jsx
+++ src/components/FeedPosts/FeedFooter.jsx
@@ -1,7 +1,62 @@
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
 
-const FeedFooter = () => {
+const FeedFooter = ({likes}) => {
+  const [liked, setLiked] = useState(false)
+  const [like, setLikes] = useState(1000)
+  const handledLike = () => {
+    setLiked(!liked)
+    if (liked) {
+      setLikes(false)
+      setLikes(like - 1)
+    } else {
+      setLikes(true)
+      setLikes(like + 1)
+    }
+  }
   return (
-    <div>FeedFooter</div>
+    <Box mb={10}>
+      <Flex alignItems={ 'center' } gap={ 4 } w={ 'full' } pt={ 0 } mb={ 2 } mt={ 4 }>
+        <Box onClick={ handledLike }
+          cursor={ 'pointer' }
+          fontSize={18}
+        >
+{!liked ?(<NotificationsLogo />) : ( <UnlikeLogo />)}
+        </Box>
+        <Box cursor={ 'pointer' } fontSize={18}>
+          <CommentLogo/>
+        </Box>
+      </Flex>
+      <Text fontWeight={600} fontSize={'sm'}>
+        {likes} likes
+      </Text>
+      <Text fontSize={ 'sm' } fontWeight={ 700 }>
+        Newton Kamau {''}
+        <Text as={ 'span' } fontWeight={ 400 }>
+          Feeling  good
+        </Text>
+
+      </Text>
+      <Text fontSize={ 'sm' } color={ 'gray' } >
+        View all 1,000 comments
+      </Text>
+      <Flex alignItems={'center'} gap={2} justifyContent={'space-between'}>
+        <InputGroup>
+          <Input variant={ 'flushed' } placeholder="add a comment...." fontSize={ 14 } />
+          <InputRightElement>
+            <Button
+              fontSize={ 14 }
+              color={ 'blue.500' }
+              fontWeight={ 600 }
+              cursor={ 'pointer' }
+              _hover={ { color: "white" } }
+              bg={'transparent'}
+            >Post</Button>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </Box>
   )
 }
 
