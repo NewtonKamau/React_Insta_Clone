67cc5bbb0718aad58157d6dac4722cc397c4b116
NewtonKamau <kamaunewton78@gmail.com>
2024-01-12 10:59:08 +0300

    profile header with dummy data

---
+++ src/components/Profile/ProfileHeader.jsx
@@ -0,0 +1,55 @@
+import { Avatar, AvatarGroup, Flex, VStack,Text, Button } from "@chakra-ui/react";
+
+const ProfileHeader = () => {
+  return <Flex gap={{base:4,sm:10}} py={10} direction={{base:'column',sm:'row'}}>
+    <AvatarGroup 
+      size={ { base: 'xl', md: '2xl' } }
+      justifySelf={ 'center' }
+      alignSelf={'flex-start '}
+    >
+      <Avatar name="NewtonKamau" src='./profilepic.jpg' alt='NewtonKamau'/>
+    </AvatarGroup>
+    <VStack
+      alignItems={ 'start' }
+      gap={ 2 }
+      mx={ 'auto' }
+      flex={1}
+    >
+      <Flex
+        gap={ 4 }
+        direction={ { base: 'column', sm: 'row' } }
+        justifyContent={ { base: 'center', sm: 'flex-start' } }
+        alignItems={ 'center' }
+        w={ 'full' }
+      >
+        <Text fontSize={ { base: 'sm', md: 'lg' } }>NewtonKamau</Text> 
+        <Flex gap={ 4 } alignItems={ 'center' } justifyContent={ 'center' }>
+          <Button bg={ 'white' } color={ 'black' } _hover={ { bg: 'whiteAlpha.800' } } size={{base:'xs',md:'sm'}}>Edit Profile</Button>
+        </Flex> 
+      </Flex>
+      <Flex alignItems={ 'center' } gap={ { base: 2, sm: 4 } } >
+        <Text fontSize={{base:'xs',md:'sm'}}>
+        <Text as={'span'} fontWeight={'bold'} mr={1} >5
+        </Text>
+          Posts
+        </Text>
+        <Text fontSize={{base:'xs',md:'sm'}}>
+        <Text as={'span'} fontWeight={'bold'} mr={1} >124
+        </Text>
+          Followers
+        </Text>
+        <Text fontSize={{base:'xs',md:'sm'}}>
+        <Text as={'span'} fontWeight={'bold'} mr={1} >13
+        </Text>
+          Following
+        </Text>
+      </Flex>
+      <Flex alignItems={ 'center' } gap={ 4 }>
+        <Text fontSize={'sm'} fontWeight={'bold'}>NewtonKamau</Text>
+      </Flex>
+        <Text fontSize={'sm'} >Learn,Code,Debug,Repeat.</Text>
+    </VStack>
+  </Flex>
+}
+
+export default ProfileHeader
